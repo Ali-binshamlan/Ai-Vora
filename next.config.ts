624b168c63c4970f11ffbcc607c13ad80d065007
Ali <ali.binshamlan12@gmail.com>
2025-11-15 15:48:25 +0300

    home

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+const nextConfig = {
+  compiler: {
+    styledComponents: true,
+  },
 };
 
 export default nextConfig;
+
